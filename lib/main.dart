63b9f9f6a658bb5024d4379ea52ea033f100b685
guptaadi123 <44012120+guptaadi123@users.noreply.github.com>
2020-10-06 19:37:50 +0530

    Flutter animation app
    created flutter animation with swipe design

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("animation"),
+        ),
+        body: LiquidSwipe(
+          pages: pages,
+          enableLoop: true,
+          fullTransitionValue: 300,
+          enableSlideIcon: true,
+          waveType: WaveType.liquidReveal,
+          positionSlideIcon: 0.5,
+        ),
+      ),
+
+    );
+  }
+
+  final pages = [
+    Container(color: Colors.white,),
+    Container(color: Colors.blue),
+    Container(color: Colors.black),
+  ];
+}
